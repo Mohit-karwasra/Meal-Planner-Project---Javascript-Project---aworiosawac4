743a5b5af9113f78afd668ce99a95a3aad7ab847
Mohit-karwasra <97739963+Mohit-karwasra@users.noreply.github.com>
2023-06-04 22:29:45 +0530

    Added calorie calculation
    according to height, weight, age, gender and activity level calculate calories.

--- App.js
+++ App.js
@@ -8,15 +8,39 @@ form.addEventListener("submit", (event) => {
 	event.preventDefault(); // Prevent form submission
 
 	// Get user input values
-	const height = document.querySelector("#input-row-1 input:nth-child(1)").value;
-	const weight = document.querySelector("#input-row-1 input:nth-child(2)").value;
-	const age = document.querySelector("#input-row-1 input:nth-child(3)").value;
+	const height = parseFloat(document.querySelector("#input-row-1 input:nth-child(1)").value);
+	const weight = parseFloat(document.querySelector("#input-row-1 input:nth-child(2)").value);
+	const age = parseInt(document.querySelector("#input-row-1 input:nth-child(3)").value);
 	const gender = document.querySelector("#gender").value;
-	const activityLevel = document.querySelector("#input-row-2 select").value;
+	const activityLevel = getActivityLevelValue(document.querySelector("#input-row-2 select").value);
+
+	function getActivityLevelValue(activityLevel) {
+		switch (activityLevel) {
+			case "low":
+				return 1.2;
+			case "medium":
+				return 1.5;
+			case "high":
+				return 1.8;
+			default:
+				return 1.5; // Default to medium activity level if no match found
+		}
+	}
+
+	// Calculate target calorie count
+	let targetCalories;
+
+	if (gender === "male") {
+		targetCalories = Math.round(88.362 + 13.397 * weight + 4.799 * height - 5.677 * age);
+	} else {
+		targetCalories = Math.round(447.593 + 9.247 * weight + 3.098 * height - 4.33 * age);
+	}
+
+	targetCalories = Math.round(targetCalories * activityLevel);
 
 	// Make API request to generate meals
 	fetch(
-		`https://api.spoonacular.com/mealplanner/generate?apiKey=7f1c356d65a84b2fa5b5901791ff99d5&timeFrame=day&targetCalories=2000&diet=balanced&height=${height}&weight=${weight}&age=${age}&gender=${gender}&activityLevel=${activityLevel}`
+		`https://api.spoonacular.com/mealplanner/generate?apiKey=7f1c356d65a84b2fa5b5901791ff99d5&timeFrame=day&targetCalories=${targetCalories}&diet=balanced&height=${height}&weight=${weight}&age=${age}&gender=${gender}&activityLevel=${activityLevel}`
 	)
 		.then((response) => response.json())
 		.then((data) => {
@@ -89,7 +113,7 @@ function updateMealCard(card, meal, calories) {
 				section3.appendChild(recipeDetails);
 
 				// Disable recipe button after showing the details
-				recipeButton.disabled = true;
+				// recipeButton.disabled = true;
 			});
 		})
 		.catch((error) => console.log(error));
